584bdc0b703452b052bf8be9744744d1a2ee0890
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-03-02 14:16:13 -0500

    Assignment complete!

--- src/Main.java
+++ src/Main.java
@@ -18,22 +18,29 @@ public class Main {
         System.out.printf("%5s%15s%15s\n", "die1", "die2", "Sum");
         System.out.printf("%5s%15s%15s\n", die1, die2, crapsRoll);
         if (crapsRoll == 2 || crapsRoll == 3 || crapsRoll == 12) {
-            System.out.println("Yikes! You crapped out. You rolled a sum of " + crapsRoll + "! Sad loss. Do you want to play again?");
+            System.out.println("Yikes! You crapped out. You rolled a sum of " + crapsRoll + "! Sad loss.");
         } // instant lose: 2, 3, 12
         else if (crapsRoll == 7 || crapsRoll == 11) {
-            System.out.println("Awesome! You got a natural, a sum of " + crapsRoll + "! That's a win! Do you want to play again?");
+            System.out.println("Awesome! You got a natural, a sum of " + crapsRoll + "! That's a win!");
         }
         // instant win: 7 (first time), 11
         else {
             thePoint = crapsRoll;
-            System.out.println("The goal to win- the point- is now " + thePoint + "! No 7 allowed!");
-            while (!crapsRoll = thePoint || !crapsRoll = 7) {
+            System.out.println("The goal to win is ''the point,'' which is now " + thePoint + "! No 7 allowed!");
+            do {
                 die1 = generator.nextInt(6) + 1;
                 die2 = generator.nextInt(6) + 1;
                 crapsRoll = die1 + die2;
                 System.out.printf("%5s%15s%15s\n", die1, die2, crapsRoll);
+            } while (crapsRoll != thePoint && crapsRoll != 7);
+
+            if (crapsRoll == thePoint) {
+                System.out.println("You met the point, " + thePoint + "! You win!");
+            } // winning with the point
+            else {
+                System.out.println("Yikes! Looks like you got a " + crapsRoll + " before " + thePoint + "! Sad loss!");
             }
-            // if
+                // losing by getting seven
         }
         // "the point" or lose with 7.
 
